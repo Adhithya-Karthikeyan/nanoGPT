e5bd8a784d9510bfaa58c8441aaa504eb683ab0a
Adhithya-Karthikeyan <81232373+Adhithya-Karthikeyan@users.noreply.github.com>
2023-05-25 08:25:57 +0530

    Added a comment

--- bench.py
+++ bench.py
@@ -18,7 +18,6 @@ bias = True
 seed = 1337
 device = 'cuda' # examples: 'cpu', 'cuda', 'cuda:0', 'cuda:1', etc.
 dtype = 'bfloat16' # 'float32' or 'bfloat16' or 'float16'
-
 compile = True # use PyTorch 2.0 to compile the model to be faster
 exec(open('configurator.py').read()) # overrides from command line or config file 
 # ---------------------------------------------------------------------------------- 
